ee9fc92e1dc6926c92d890f3a1ceaa188831fcee
soumyasootar <soumyaswaroopsootar@gmail.com>
2023-03-30 21:15:50 +0530

    Animation

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { motion } from "framer-motion";
 
-const MotionLink=motion(Link)
+const MotionLink = motion(Link);
 
 const Logo = () => {
   return (
@@ -10,7 +10,18 @@ const Logo = () => {
       <MotionLink
         href={"/"}
         className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold"
-      whileHover={{scale:1.5}}
+        whileHover={{
+          scale: 1.05,
+          backgroundColor: [
+            "#121212",
+            "rgba(131,58,180,1)",
+            "rgba(253,29,29,1)",
+            "rgba(252,176,69,1)",
+            "rgba(131,58,180,1)",
+            "#121212",
+          ],
+          transition:{duration:1,repeat:Infinity}
+        }}
       >
         SSS
       </MotionLink>
