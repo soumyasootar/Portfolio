8b7a695cdfab5c00152ce061462d95dc8d8fef1c
soumyasootar <soumyaswaroopsootar@gmail.com>
2023-04-13 19:38:31 +0530

    pdf updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/Soumya_Sootar.pdf"
+                  href="/Soumya_Swaroop Sootar_Resume.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2 px-6 rounded-lg text-lg font-semibold dark:text-dark dark:bg-light hover:dark:bg-dark hover:dark:text-light hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark hover:dark:border-light md:p-2 md:px-4 md:text-base"
                   download={true}
